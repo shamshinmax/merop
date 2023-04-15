e3c7cf0562f8f4572e0c480d88bcfab2d655a2b8
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-15 10:47:03 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -2,7 +2,13 @@ let tg = window.Telegram.WebApp;
 tg.setBackgroundColor("#ffffff")
 
 let btn = document.getElementById("btn");
+document.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+        e.preventDefault();
+        return false;
+    }
 
+})
 btn.addEventListener("click", function(){
     let mest = document.getElementById("mest").value;
     let krat = document.getElementById("krat").value;
