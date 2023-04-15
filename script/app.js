96a16a3a7c813bf217bd4c047f8bde61c0c84f09
shamshinmax <94185311+shamshinmax@users.noreply.github.com>
2023-04-15 10:02:42 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,26 +1,13 @@
 let tg = window.Telegram.WebApp;
 tg.setBackgroundColor("#ffffff")
 
-const date = new Date();
-const timeControl = document.querySelector('input[type="time"]');
-let hours = date.getHours();
-let minutes = date.getMinutes();;
-if (date.getMinutes() < 10){
-   minutes = "0" + date.getMinutes();
-}
-if (date.getHours() < 10){
-   hours = "0" + date.getHours();
-}
-var time = hours + ":" + minutes
-timeControl.value = hours + ":" + minutes;
 let btn = document.getElementById("btn");
-const times = document.getElementById("startTime");
-times.addEventListener("input", () => {
-    time = times.value;
-}, false);
-
 
 btn.addEventListener("click", function(){
-    var place = document.getElementById("selection").value;
-    tg.sendData(time+" "+place+" "+tg.colorScheme);
+    let mest = document.getElementById("mest").value;
+    let krat = document.getElementById("krat").value;
+    let post = document.getElementById("post").value;
+    if (mest != '' && krat != '' && post != '')
+
+        tg.sendData(time+" "+place+" "+tg.colorScheme);
 });
